a9e5d12c4f7419e7cb2ca4070e422b3a1137160b
YagoGu <yago.gutierrez15@gmail.com>
2023-08-03 15:52:44 +0200

    form to add people to our server

--- app.js
+++ app.js
@@ -21,3 +21,22 @@ app.get('/api/:id', (req, res) => {
     const id = req.params.id;
     res.send(data[id]);
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/views/form.html")
+});
+
+//allows us to see things of our body page
+const bodyParser = require('body-parser'); 
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+app.post('/post', (req, res) => {
+    const newPerson = {
+        "id" : data.length+1,
+        "name" : req.body.name,
+        "age" : parseInt(req.body.age),
+        "occupation" : req.body.occupation
+    }
+        data.push(newPerson);
+    res.redirect("/api/all")
+});
